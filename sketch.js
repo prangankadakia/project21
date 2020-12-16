ea27cad43acae1d97ef99966446c25ee713156f8
prangankadakia <75072662+prangankadakia@users.noreply.github.com>
2020-12-16 16:07:33 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,16 +4,18 @@ var bullet,speed,weight;
 
 function setup() {
   createCanvas(1600, 400);
-
+	
+thickness=random(22,83)
+speed=random(223,321)
+weight=random(30,52)
+	
 bullet=createSprite(200,100)
 bullet.shapeColor="white"
 
 wall=createSprite(1200, 200, thickness, height/2)
 waqll.shapeColor=color(80,80,80)
 
-thickness=random(22,83)
-speed=random(223,321)
-weight=random(30,52)
+
 }
 
 
@@ -24,7 +26,7 @@ function draw() {
   {
 	  bullet.velocityX=0; 
 	  var damage=0.5 * weight * speed * speed/(thickness *thickness* thickness);
-  }
+  
   if(damage>10)
 	{
  wall.shapeColor=color(255,0,0);
@@ -34,7 +36,8 @@ function draw() {
   {
 	  wall.shapeColor=color(0,255,0);
   }
-  
+  }
+	
   drawSprites();
  
 }
